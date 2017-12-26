bd24f3a4cbbdcad5c23a6b3d78177db8086b63b3
TheDeadFish <michaelstamper1@gmail.com>
2017-12-26 05:01:07 +0000

    1. implemented lag corrected tas video playback

--- sonic.asm
+++ sonic.asm
@@ -15,7 +15,7 @@ EnableSRAM:	equ 0	; change to 1 to enable SRAM
 BackupSRAM:	equ 1
 AddressSRAM:	equ 3	; 0 = odd+even; 2 = even only; 3 = odd only
 
-Revision:	equ 0	; change to 1 for JP1 revision
+Revision:	equ 1	; change to 1 for JP1 revision
 
 ZoneCount:	equ 6	; discrete zones are: GHZ, MZ, SYZ, LZ, SLZ, and SBZ
 
@@ -506,7 +506,7 @@ VBla_Music:
 		jsr	(UpdateMusic).l
 
 VBla_Exit:
-		addq.l	#1,(v_vbla_count).w
+		;addq.l	#1,(v_vbla_count).w
 		movem.l	(sp)+,d0-a6
 		rte	
 ; ===========================================================================
@@ -855,24 +855,23 @@ JoypadInit:
 
 ReadJoypads:
 		lea	(v_jpadhold1).w,a0 ; address where joypad states are written
-		lea	($A10003).l,a1	; first	joypad port
-		bsr.s	@read		; do the first joypad
-		addq.w	#2,a1		; do the second	joypad
 
 	@read:
-		move.b	#0,(a1)
-		nop	
-		nop	
-		move.b	(a1),d0
-		lsl.b	#2,d0
-		andi.b	#$C0,d0
-		move.b	#$40,(a1)
-		nop	
-		nop	
-		move.b	(a1),d1
-		andi.b	#$3F,d1
-		or.b	d1,d0
-		not.b	d0
+		; read count delta
+		move.l ($FFFFFFC4).w, a1
+		add.l #EndOfRom, a1
+		moveq #0, d1
+		move.b (a1)+,d1
+		
+		; apply count delta
+		beq.s @skip
+		addq.l #2, ($FFFFFFC4).w
+		subq.b #1, d1
+@skip: 	addq.b #1, d1
+		add.l d1, (v_vbla_count).w
+		
+		; update controller
+		move.b (a1), d0		
 		move.b	(a0),d1
 		eor.b	d0,d1
 		move.b	d0,(a0)+
@@ -9125,6 +9124,6 @@ SoundDriver:	include "s1.sounddriver.asm"
 ; end of 'ROM'
 		even
 EndOfRom:
-
+	incbin "demo_out.bin"
 
 		END
