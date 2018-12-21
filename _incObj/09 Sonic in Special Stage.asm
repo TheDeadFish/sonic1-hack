36fd72c27ca972c685af77e307cb944798ccd5d1
TheDeadFish <michaelstamper1@gmail.com>
2018-12-21 07:58:21 +0000

    1. special stage, added option for fixed jump physics

--- _incObj/09 Sonic in Special Stage.asm
+++ _incObj/09 Sonic in Special Stage.asm
@@ -55,6 +55,9 @@ Obj09_Modes:	dc.w Obj09_OnWall-Obj09_Modes
 ; ===========================================================================
 
 Obj09_OnWall:
+	if SS_JumpFix>0
+		bclr	#7,obStatus(a0)	; clear "Sonic has jumped" flag
+	endc
 		bsr.w	Obj09_Jump
 		bsr.w	Obj09_Move
 		bsr.w	Obj09_Fall
@@ -62,7 +65,7 @@ Obj09_OnWall:
 ; ===========================================================================
 
 Obj09_InAir:
-		bsr.w	nullsub_2
+		bsr.w	Obj09_JumpHeight
 		bsr.w	Obj09_Move
 		bsr.w	Obj09_Fall
 
@@ -224,6 +227,10 @@ Obj09_Jump:
 		asr.l	#8,d0
 		move.w	d0,obVelY(a0)
 		bset	#1,obStatus(a0)
+	if SS_JumpFix>0
+		bset	#7,obStatus(a0)	; set "Sonic has jumped" flag
+	endc
+		
 		sfx	sfx_Jump,0,0,0	; play jumping sound
 
 Obj09_NoJump:
@@ -231,27 +238,51 @@ Obj09_NoJump:
 ; End of function Obj09_Jump
 
 
-; ||||||||||||||| S U B	R O U T	I N E |||||||||||||||||||||||||||||||||||||||
-
-
-nullsub_2:
-		rts	
-; End of function nullsub_2
-
 ; ===========================================================================
 ; ---------------------------------------------------------------------------
-; unused subroutine to limit Sonic's upward vertical speed
+; Subroutine to limit Sonic's upward vertical speed
 ; ---------------------------------------------------------------------------
-		move.w	#-$400,d1
-		cmp.w	obVelY(a0),d1
-		ble.s	locret_1BBB4
-		move.b	(v_jpadhold2).w,d0
+
+; ||||||||||||||| S U B	R O U T	I N E |||||||||||||||||||||||||||||||||||||||
+
+Obj09_JumpHeight:			; XREF: Obj09_InAir
+	if SS_JumpFix>0
+		move.b	(v_jpadhold2).w,d0	; is the jump button up?
 		andi.b	#btnABC,d0
-		bne.s	locret_1BBB4
-		move.w	d1,obVelY(a0)
+		bne.s	locret_1BBB4		; if not, branch to return
+		btst	#7,obStatus(a0)		; did Sonic jump or is he just falling or hit by a bumper?
+		beq.s	locret_1BBB4		; if not, branch to return
+		move.b	(v_ssangle).w,d0	; get SS angle
+		andi.b	#$FC,d0
+		neg.b	d0
+		subi.b	#$40,d0
+		jsr	(CalcSine).l			
+		move.w	obVelY(a0),d2		; get Y speed
+		muls.w	d2,d0			; multiply Y speed by sin
+		asr.l	#8,d0			; find the new Y speed
+		move.w	obVelX(a0),d2		; get X speed
+		muls.w	d2,d1			; multiply X speed by cos
+		asr.l	#8,d1			; find the new X speed
+		add.w	d0,d1			; combine the two speeds
+		cmpi.w	#$400,d1		; compare the combined speed with the jump release speed
+		ble.s	locret_1BBB4		; if it's less, branch to return
+		move.b	(v_ssangle).w,d0
+		andi.b	#$FC,d0
+		neg.b	d0
+		subi.b	#$40,d0
+		jsr	(CalcSine).l
+		muls.w	#$400,d1
+		asr.l	#8,d1
+		move.w	d1,obVelX(a0)
+		muls.w	#$400,d0
+		asr.l	#8,d0
+		move.w	d0,obVelY(a0)		; set the speed to the jump release speed
+		bclr	#7,obStatus(a0)		; clear "Sonic has jumped" flag
 
 locret_1BBB4:
-		rts	
+	endc
+		rts
+		
 ; ---------------------------------------------------------------------------
 ; Subroutine to	fix the	camera on Sonic's position (special stage)
 ; ---------------------------------------------------------------------------
@@ -614,6 +645,9 @@ Obj09_ChkBumper:
 		asr.l	#8,d0
 		move.w	d0,obVelY(a0)
 		bset	#1,obStatus(a0)
+	if SS_JumpFix>0
+		bclr	#7,obStatus(a0)	; clear "Sonic has jumped" flag
+	endc	
 		bsr.w	SS_RemoveCollectedItem
 		bne.s	Obj09_BumpSnd
 		move.b	#2,(a2)
